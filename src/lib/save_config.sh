ddc6d6c3f55a32a42b2c67ba332c1900de3a9ae1
Danny Ben Shitrit <db@dannyben.com>
2020-10-14 11:48:55 +0000

    - Fix post-save instructions in cases when ALF_ALIASES_FILE is set

--- src/lib/save_config.sh
+++ src/lib/save_config.sh
@@ -1,9 +1,11 @@
 save_config() {
   find_config
 
+  friendly_aliases_file="${aliases_file/#$HOME/'~'}"
+
   echo "Saving to $aliases_file"
   generate_config > "$aliases_file"
 
   echo "To apply the new aliases to the current session, run:"
-  echo "$ source ~/.bash_aliases"
+  echo "$ source $friendly_aliases_file"
 }
