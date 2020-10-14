f9d812d9651e8bfda8818de71bc50aecb25d091a
Danny Ben Shitrit <db@dannyben.com>
2020-10-14 10:34:48 +0000

    complete alf info command

--- src/info_command.sh
+++ src/info_command.sh
@@ -1,12 +1,46 @@
-inforow() {
-  printf "%-30s %s\n" "$1:" "$(green "$2")"
-}
+# inforow() {
+#   printf "%-30s %s\n" "$1:" "$2"
+# }
 
 find_config
 
-inforow "executable / path" "$(which alf)"
-inforow "env / ALF_RC_FILE" "${ALF_RC_FILE:-unset}"
-inforow "env / ALF_ALIASES_FILE" "${ALF_ALIASES_FILE:-unset}"
-inforow "repo / path" "$repo_path"
-inforow "config / path" "$config_file"
+if [[ -d "$repo_path/.git" ]]; then
+  pushd "$repo_path" > /dev/null
+  remote="$(git config --get remote.origin.url)"
+  popd > /dev/null
+else
+  remote="unset"
+fi
 
+if [[ -f "$rc_file" ]]; then
+  alfrc_content="$(<"$rc_file")"
+else
+  alfrc_content="does not exist"
+fi
+
+if [[ -f "$aliases_file" ]]; then
+  bash_aliases_status="exists"
+else
+  bash_aliases_status="does not exist"
+fi
+
+echo "Executable:"
+echo "  path:              $(which alf)"
+echo 
+echo "Environment:"
+echo "  ALF_RC_FILE:       ${ALF_RC_FILE:-unset}"
+echo "  ALF_ALIASES_FILE:  ${ALF_ALIASES_FILE:-unset}"
+echo 
+echo "Paths:"
+echo "  alfrc path:        $rc_file"
+echo "  aliases path:      $aliases_file"
+echo "  repo path:         $repo_path"
+echo "  config path:       $config_file"
+echo 
+echo "Files:"
+echo "  alfrc:             $alfrc_content"
+echo "  aliases:           $bash_aliases_status"
+echo 
+echo "GitHub:"
+echo "  remote:            $remote"
+echo 
